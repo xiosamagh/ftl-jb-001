bccf9eed861970fe3f691810b3ca3575ed7278db
Ilya <92110133+xiosamagh@users.noreply.github.com>
2022-06-14 17:17:38 +0300

    T002 - Task2

---
+++ src/com/tasks/T002/Task2.java
@@ -0,0 +1,16 @@
+package com.tasks.T002;
+
+import com.tasks.T001.Task1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Task2.show();
+    }
+
+    static void show() {
+        for (int i = 5; i > 0; i--) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+}
