7a760381ac49203a46d98d0cd1bda6013106b9c9
Ilya <92110133+xiosamagh@users.noreply.github.com>
2022-06-14 16:46:04 +0300

    L003 - Animal and Cat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,14 @@ package com.company;
 
 import com.company.L001.HelloWorld;
 import com.company.L002.POJO;
+import com.company.L003.Cat;
 
 public class Main {
 
     public static void main(String[] args) {
         POJO.run();
+
+        Cat cat = new Cat();
+        cat.voice();
     }
 }
