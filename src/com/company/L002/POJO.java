c39e92e0fd4e8e871800d4e56baea9e447b53778
Ilya <92110133+xiosamagh@users.noreply.github.com>
2022-06-14 16:38:34 +0300

    L002 - vars

---
+++ src/com/company/L002/POJO.java
@@ -0,0 +1,15 @@
+package com.company.L002;
+
+public class POJO {
+    public static void run() {
+        int x = 0;
+        long i = 100l, j = 200l, k = 300l;
+        byte a1 = 0x06, a2 = 0x07;
+        short r1 = 017;
+
+        System.out.println(x);
+        System.out.println(i + " " + j + " " +  k + " ");
+        System.out.println(a1);
+        System.out.println(r1);
+    }
+}
