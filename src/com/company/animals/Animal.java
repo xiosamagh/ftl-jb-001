b27e60963992c2bf9b81df8dd957813f984bc543
Ilya <92110133+xiosamagh@users.noreply.github.com>
2022-06-15 18:14:47 +0300

    L006 - Static/Instance methods

--- src/com/company/animals/Animal.java
+++ src/com/company/animals/Animal.java
@@ -3,6 +3,7 @@ package com.company.animals;
 public class Animal {
     protected String name;
     protected Integer weight;
+    protected Integer currentPosition = 0;
 
     public Animal(String name, Integer weight) {
         this.name = name;
@@ -14,5 +15,33 @@ public class Animal {
 
     public void voice() {
         System.out.println("Voice!");
+    }
+
+    protected void runForward(Integer length) {
+        this.currentPosition = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Integer getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(Integer currentPosition) {
+        this.currentPosition = currentPosition;
     }
 }
