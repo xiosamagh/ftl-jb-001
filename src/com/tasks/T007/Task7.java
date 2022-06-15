176c7793e48e6aef7e2ee4bc6dc978be307aa3b7
Ilya <92110133+xiosamagh@users.noreply.github.com>
2022-06-15 11:35:21 +0300

    T007 - Task7

---
+++ src/com/tasks/T007/Task7.java
@@ -0,0 +1,13 @@
+package com.tasks.T007;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Task7.run();
+    }
+
+    static void run() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("* * * * *");
+        }
+    }
+}
